112d9395cf234db073ed1e5335c78648be4c1dc1
Aiman Irfan <aimanirfan1017@gmail.com>
2020-09-16 17:38:40 +0800

    Updates

---
+++ precache-manifest.3275588deb67a4caa5fcd6b1485cc5ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746b1159ed28a55746e56893e83bba13",
+    "url": "/ADABatch18-react-restaurant-web/index.html"
+  },
+  {
+    "revision": "51627c6c095b51741692",
+    "url": "/ADABatch18-react-restaurant-web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25cec89cbe29df1ea869",
+    "url": "/ADABatch18-react-restaurant-web/static/js/2.27293a32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ADABatch18-react-restaurant-web/static/js/2.27293a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51627c6c095b51741692",
+    "url": "/ADABatch18-react-restaurant-web/static/js/main.0e714916.chunk.js"
+  },
+  {
+    "revision": "cdff623a209705c58eca",
+    "url": "/ADABatch18-react-restaurant-web/static/js/runtime-main.bfa369d1.js"
+  }
+]);
